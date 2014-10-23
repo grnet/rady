d88d81a3a78601bdaba51ae0c2217d2841329d49
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2014-10-23 16:54:59 +0300

    Add color inverter

--- utils.py
+++ utils.py
@@ -15,3 +15,15 @@ def _slugify(value):
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
     value = unicode(_slugify_strip_re.sub('', value).strip().lower())
     return _slugify_hyphenate_re.sub('-', value)
+
+
+def invertHex(hexNumber):
+    hexNumber = hexNumber.replace("#", "")
+    r = hexNumber[0:2]
+    inverse_r = "{0:0{1}x}".format(abs(int(r, 16) - 255),2)
+    g = hexNumber[2:4]
+    inverse_g = "{0:0{1}x}".format(abs(int(g, 16) - 255),2)
+    b = hexNumber[4:6]
+    inverse_b = "{0:0{1}x}".format(abs(int(b, 16) - 255),2)
+    return "#%s%s%s"%(inverse_r, inverse_g, inverse_b)
+
